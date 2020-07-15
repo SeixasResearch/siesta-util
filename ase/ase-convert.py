30af351d1e7a8ea40f27cc623428a8af9324bd63
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2020-07-15 01:48:33 -0300

    update ase-convert.py

--- ase/ase-convert.py
+++ ase/ase-convert.py
@@ -1,6 +1,2 @@
-import matplotlib.pyplot as plt
 from ase.io import read, write
-
-images = read('MoS2-initial.traj@-7:')
-
-write('initial.xyz',images)
+write('initial.xyz', read('MoS2-initial.traj@-7:'))
